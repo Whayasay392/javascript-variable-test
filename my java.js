6d21a44b56fc257cf2a41ed9a81f1f45ca12f339
Wasiu Olalekan <wasiuolalekan190@gmail.com>
2020-03-25 21:01:10 +0000

    First javascript

---
+++ my java.js
@@ -0,0 +1,13 @@
+let name = 'Okunola W. Olalekan';
+console.log("My name is: " + name);
+let selectedCourses = ('html', 'css', 'javascript');
+console.log(selectedCourses);
+let userName = 'Whayasay392';
+console.log(userName);
+const data = [];
+for(let i=0; i<=200; i++) {
+    if(i%2===1) {
+        data.push(i);
+    }
+}
+console.log(data);
